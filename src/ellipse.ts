d3a5d505525a4ab73f7ba23002c40ea2892260ed
fuchunhui <fuchuhu7@gmail.com>
2021-07-22 11:05:04 +0800

    feat: circle & group

--- src/ellipse.ts
+++ src/ellipse.ts
@@ -8,14 +8,20 @@
 import {Ellipse, Piece} from './types/index';
 import {formatColor} from './matics';
 
+// TODO 添加自定义前缀，否则，同一个环境多次引用，就会出现id重复的问题
+// const DEGAULT_PREFIX = 'EE';
 const RADIALGRADIENT_PREFIX = 'rg-';
 const CLIPPATH_PREFIX = 'cut-';
+const CLIPPATH_ELLIPSE = 'cut-ellipse';
 
 const createEllipse = (options: Ellipse) => {
   const {deg, rx, ry, rgr, data} = options;
-  // const colorList: string[] = data.map(item => item.color);
   
   const defsNode = _defs(options);
+
+  // const colorList: string[] = pieceList.map(item => item.color);
+  // _group
+
   const element = _svgBlock(rx, ry);
   element.appendChild(defsNode);
   return element;
@@ -31,12 +37,22 @@ const _svgBlock = (rx: number, ry: number) => {
 
 const _defs = (options: Ellipse) => {
   const ele = document.createElement('defs');
-  const {deg, rx, rgr, data: pieceList} = options;
+  const {deg, rx, ry, rgr, data: pieceList} = options;
 
-  pieceList.forEach(item => {
+  // 根据deg rx 以及percent数组，计算出 坐标点数组
+  // 根据坐标点数组，映射 path数组
+  // 然后再根据颜色 循环。
+  const percentList: (number | string)[] = pieceList.map(item => item.percent);
+  // const pointList
+  // const pathList
+
+  pieceList.forEach((item, index) => {
     ele.appendChild(_radialGradient(item.color, rgr));
-    // ele.appendChild(_clipPath(item, rgr));
+    // ele.appendChild(_clipPath(item, pathList[index]));
   })
+
+  ele.appendChild(_ellipseClipPath(rx, ry));
+
   return ele;
 };
 
@@ -61,12 +77,60 @@ const _radialGradient = (color: string, rgr: string) => {
   return ele;
 };
 
-const _clipPath = () => {
+const _clipPath = (color: string, d: string) => {
+  const ele = document.createElement('clipPath');
+  const id = formatColor(color);
+  ele.setAttribute('id', `${CLIPPATH_PREFIX}${id}`);
 
+  const path = document.createElement('path');
+  path.setAttribute('d', d);
+
+  ele.appendChild(path);
+
+  return ele;
 };
-const _ellipseClipPath = () => {};
-const _circle = () => {};
-const _group = () => {};
+
+const _ellipseClipPath = (rx: number, ry: number) => {
+  const ele = document.createElement('clipPath');
+  ele.setAttribute('id', CLIPPATH_ELLIPSE);
+
+  const ellipse = document.createElement('ellipse');
+  const r = String(rx);
+  ellipse.setAttribute('cx', r);
+  ellipse.setAttribute('cy', r);
+  ellipse.setAttribute('rx', r);
+  ellipse.setAttribute('ry', String(ry));
+
+  ele.appendChild(ellipse);
+
+  return ele;
+};
+
+const _circle = (color: string, rx: number) => {
+  const ele = document.createElement('circle');
+  const id = formatColor(color);
+  const r = String(rx);
+
+  ele.setAttribute('cx', r);
+  ele.setAttribute('cy', r);
+  ele.setAttribute('r', r);
+  ele.setAttribute('fill', `url(#${RADIALGRADIENT_PREFIX}${id})`);
+  ele.setAttribute('clip-path', `url(#${CLIPPATH_PREFIX}${id})`);
+
+  return ele;
+};
+
+const _group = (colorList: string[], rx: number, ry: number) => {
+  const ele = document.createElement('g');
+  ele.setAttribute('clip-path', `url(${CLIPPATH_ELLIPSE})`);
+  ele.setAttribute('transform', `translate(0, -${ry / 2})`);
+
+  colorList.forEach(item => {
+    ele.appendChild(_circle(item, rx))
+  });
+
+  return ele;
+};
 
 // svg 基础结构定义 rx * 2, ry * 2
 // radialGradient 递归处理，颜色 r rg + 颜色
