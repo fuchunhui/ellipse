c95b83a411c88bfc9ffeab78e7bd962e05fefc4a
fuchunhui <fuchuhu7@gmail.com>
2021-07-22 18:54:52 +0800

    feat: new demo

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,48 @@ const svg = createEllipse({
   rx: 564,
   ry: 253,
   rgr: '50%',
-  data: [ // percent 小数点最多后4位
+  // data: [ // percent 小数点最多后4位
+  //   {
+  //     color: 'green',
+  //     percent: 0.25
+  //   },
+  //   {
+  //     color: 'purple',
+  //     percent: '37.5%'
+  //   },
+  //   {
+  //     color: 'cyan',
+  //     percent: 0.225
+  //   },
+  //   {
+  //     color: '#FF0000',
+  //     percent: 0.15
+  //   }
+  // ]
+  data: [
     {
-      color: 'green',
-      percent: 0.25
+      color: '#FEF4D4',
+      percent: 0.20
     },
     {
-      color: 'purple',
+      color: '#dFE8FF',
       percent: '37.5%'
     },
     {
-      color: 'cyan',
+      color: '#DDF1FE',
       percent: 0.225
     },
     {
-      color: '#FF0000',
-      percent: 0.15
+      color: '#DFF9EF',
+      percent: 0.05
+    },
+    {
+      color: '#D7F5F7',
+      percent: 0.10
+    },
+    {
+      color: '#F5E7FE',
+      percent: 0.05
     }
   ]
 });
